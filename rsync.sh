2904ea51f1c334d76c9634edfa31ba2d2a107635
gnostic <64601257+nynxa@users.noreply.github.com>
2024-09-07 15:55:43 -0400

    release: v1.0.0 feat: add ability to place todos under a note heading. chore: replace copy-plugin.sh with plugin for esbuild using bun shell chore: better settings validation

--- rsync.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cp -Rf \
-    ~/code/plugins/obsidian/obsidian-todo-extractor '/Users/divinelight/Library/Mobile Documents/iCloud~md~obsidian/Documents/.obsidian/plugins' &&
-    echo "copied files to obsidian-todo-extractor"
